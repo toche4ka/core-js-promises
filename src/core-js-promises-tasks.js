13ebcaf296cad77965c614252b5d302fbbc90ba4
Anna Tochk <anitochka1105@gmail.com>
2025-04-15 04:27:42 +0500

    feat: add solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -40,13 +40,7 @@ function getPromise(number) {
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
 function getPromiseResult(source) {
-  return new Promise((resolve, reject) => {
-    if (source) {
-      resolve('success');
-    } else {
-      reject(new Error('fail'));
-    }
-  });
+  return source.then(() => 'success').catch(() => 'fail');
 }
 
 /**
